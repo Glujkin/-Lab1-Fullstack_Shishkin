78724e776ec8b5503d63463594dda49c413c6e98
TishkaaPlay <tix.tot@yandex.ru>
2026-01-16 11:32:24 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Шишкин Т.А,НовиковД.Р");
+DateTime now = DateTime.Now;
+Console.WriteLine($"Текущая дата и время: {now}");
